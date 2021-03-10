76f923baafecbf99a83e3829586d846de7a78a87
ALisher <alishka1216@gmail.com>
2021-03-10 19:54:46 +0600

    добавил html

---
+++ source/webapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.core.validators import MinValueValidator
+from webapp.models import Status, Type
+
+
+class TrackerForm(forms.Form):
+
+    title = forms.CharField(max_length=120, required=True)
+    detail_description = forms.CharField(max_length=3000, required=True, widget=forms.Textarea)
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True,  initial='new')
+    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=False,  initial='task')
