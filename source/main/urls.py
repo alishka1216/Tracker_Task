670abcdd220b3b67498d384ab4ff340c7667f4ce
ALisher <alishka1216@gmail.com>
2021-03-10 18:34:06 +0600

    первый комит

--- source/main/urls.py
+++ source/main/urls.py
@@ -1,21 +1,20 @@
-"""main URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from webapp.views import (
+    IndexView,
+    ArticleView,
+    RedirectView,
+    article_create_view,
+    article_update_view,
+    article_delete_view,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='tracker-list'),  # URL для отображения списка статей
+    path('articles/add/', tracker_create_view, name='tracker-add'),  # URL для создания статьи
+    path('article/<int:pk>/', TrackerleView.as_view(), name='tracker-view'),  # URL для просмотра деталей статьи
+    path('article/update/<int:pk>/', tracker_update_view, name='tracker-update'),
+    path('article/delete/<int:pk>/', tracker_delete_view, name='tracker-delete')
 ]
